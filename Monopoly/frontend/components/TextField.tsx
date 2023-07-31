1f07a732aadb10f5ae3663e4751101f3c2c3b262
Andy Shi <ashi2@mail.sfsu.edu>
2023-07-31 14:41:13 -0700

    Textfield props

--- Monopoly/frontend/components/TextField.tsx
+++ Monopoly/frontend/components/TextField.tsx
@@ -2,17 +2,18 @@ import {HTMLInputTypeAttribute} from "react";
 
 type TextFieldProps = {
     inputType?: HTMLInputTypeAttribute;
-    value: String;
-    error: Boolean;
-    disabled: Boolean;
-    required: Boolean;
+    value?: string;
+    error?: boolean;
+    disabled?: boolean;
+    required?: boolean;
+    placeholder?: string;
 }
 
 export function TextField (props: TextFieldProps): JSX.Element {
     return (
         <>
             <label>
-                <input type={props.inputType} />
+                <input type={props.inputType || "text"} disabled={props.disabled} required={props.required} placeholder={props.placeholder}/>
             </label>
         </>
     );

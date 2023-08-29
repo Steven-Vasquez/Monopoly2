b5ec6de8fa83cfeec92ef2a43c03957419e854b7
Andy Shi <ashi2@mail.sfsu.edu>
2023-08-28 17:30:21 -0700

    Fixed textfield overflow, adjusted styling.

--- frontend/components/TextField.tsx
+++ frontend/components/TextField.tsx
@@ -18,8 +18,8 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 export function TextField ({label, ...props}: TextFieldProps): JSX.Element {
     return (
         <>
-            <label className={(props.value) ? "filled" : "label-class"}>
-                <span className="text-field-span">{label}</span>
+            <label className={"input-container " + ((props.value) ? "filled" : "")}>
+                <span className="text-field-label">{label}</span>
                 <input className="text-field-input" {...props}/>
             </label>
         </>

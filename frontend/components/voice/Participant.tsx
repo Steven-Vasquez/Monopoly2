edf694d8d1e7d0a928cb7e3fc1192c1d7bd7b6ab
steven vasquez <ministeven10@gmail.com>
2023-09-05 19:05:48 -0700

    Now Participant.tsx should be fullproof implemented. TODO: set up VoiceChatRoom to manage participants and emit to specific participants when joining/leaving/muting/unmuting

--- frontend/components/voice/Participant.tsx
+++ frontend/components/voice/Participant.tsx
@@ -20,24 +20,49 @@ const Participant: React.FC<Participant> = ({ id }) => {
     const audioRef = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
+        const trackDictionary: { [key: number]: MediaStreamTrack } = {};
 
-        const trackSubscribed = (track: MediaStreamTrack) => {
-            if (track.kind === "audio") {
-                setAudioTracks((audioTracks) => [...audioTracks, track]);
-            }
+        // Define a function to handle when a new audio track is subscribed to (AKA when a user joins the chat or unmutes their mic)
+        const trackSubscribed = () => {
+            // Capture audio from the user's microphone when a new audio track is subscribed.
+            navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then((stream) => {
+                    // Get the audio tracks from the stream
+                    const audioTrack = stream.getAudioTracks()[0];
+
+                    // Check if there are any audio tracks in the stream
+                    if (audioTrack) {
+                        // Add the first audio track to the audioTracks state
+                        setAudioTracks((audioTracks) => [...audioTracks, audioTrack]);
+
+                        // Define trackDictionary as an object where keys are numbers and values are MediaStreamTrack instances
+                        const trackDictionary: { [key: number]: MediaStreamTrack } = {};
+                        trackDictionary[id] = audioTrack;
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error accessing microphone:", error);
+                });
+
         };
 
+        // Define a function to handle when an audio track is unsubscribed from (AKA when a user leaves the chat or mutes their mic)
         const trackUnsubscribed = (track: MediaStreamTrack) => {
             if (track.kind === "audio") {
                 setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track));
             }
         };
 
-        socket.on(`audioTrackAdded/${id}`, trackSubscribed);
-        socket.on(`audioTrackRemoved/${id}`, trackUnsubscribed);
+        socket.on(`audioTrackAdded/${id}`, trackSubscribed); // When websocket emits that a user joins the chat, start broadcasting their audio
+        socket.on(`audioTrackRemoved/${id}`, () => { // When websocket emits that a user leaves the chat, stop broadcasting their audio
+            if (trackDictionary[id]) {
+                trackUnsubscribed(trackDictionary[id]);
+                delete trackDictionary[id];
+            }
+        });
 
-
-        return () => {
+        return () => { // Clean up
             setAudioTracks([]);
             // Clean up event listeners
             socket.off(`audioTrackAdded/${id}`, trackSubscribed);

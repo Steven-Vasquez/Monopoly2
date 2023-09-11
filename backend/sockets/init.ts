da9dc8cf933f0d823e0a36264e40cd0139efb00c
steven vasquez <ministeven10@gmail.com>
2023-09-11 16:13:45 -0700

    Set up websocket server to handle WebRTC voice chat

--- backend/sockets/init.ts
+++ backend/sockets/init.ts
@@ -3,13 +3,13 @@ import { Server } from "socket.io";
 import { Express } from "express";
 
 // Create a new interface that extends the http.Server interface to include the io property to be used elsewhere (to access io in backend, for example)
-interface SocketServer extends http.Server { 
+interface SocketServer extends http.Server {
   io: Server;
 }
 
 const initSockets = (app: Express, sessionMiddleware: any): SocketServer => {
   const server = http.createServer(app) as SocketServer; // Cast server to SocketServer
-  
+
   // Use the cors middleware to allow WebSocket connections
   const io = new Server(server, {
     cors: {
@@ -23,12 +23,36 @@ const initSockets = (app: Express, sessionMiddleware: any): SocketServer => {
   io.on("connection", (socket) => {
     console.log("Connection", socket.id);
 
+    /**************  Game/Lobby/Chat rooms **************/
+    // When a user joins a game lobby/game room
     socket.on("join", (game_id: string) => {
       console.log(socket.id + " is joining room: " + game_id);
       socket.join(game_id);
       console.log("All socket rooms connected:");
       console.log(io.sockets.adapter.rooms);
     });
+
+
+    /**************  Voice chat rooms **************/
+
+    socket.on("joinVoiceChat", (game_id: string, data: any) => {
+
+      socket.join("voiceChat:" + game_id); // ex: voiceChat:1
+
+      // Cleanup when user leaves voice chat by disconnecting
+      socket.on("disconnect", () => {
+        socket.broadcast.to("voiceChat:" + game_id).emit("userLeftVoiceChat", socket.id);
+      });
+
+      // Send signal to other user that someone is joining voice chat
+      socket.broadcast.to("voiceChat:" + game_id).emit("offer", { signal: data.signalData, from: data.from, name: data.name });
+
+      // Send signal from users to receive the offer
+      socket.on("answer", (data: any) => {
+        socket.to(data.to).emit("callAccepted", data.signal);
+      });
+    });
+    /**********************************************/
 
     socket.on("disconnect", () => {
       console.log("Socket disconnected: " + socket.id);

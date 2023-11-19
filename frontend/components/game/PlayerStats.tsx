bfbc119c1361b195359a85bcfaf1aeb9c3e6acc9
krleong <krleong1234@gmail.com>
2023-11-19 02:28:19 -0800

    Added player stats window as designed in Figma wireframe, still WIP

---
+++ frontend/components/game/PlayerStats.tsx
@@ -0,0 +1,71 @@
+import "../../stylesheets/PlayerStats.css";
+// TODO: indicate if player is in Voice Chat (VC)
+import { PhoneCall, PhoneX, Microphone, MicrophoneSlash, SpeakerHigh, SpeakerSlash } from "@phosphor-icons/react";
+
+function PlayerStats() {
+    const playerInfo = [
+        {
+            name: "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.",
+            vc: "[VC]",
+            properties: ["4", "2", "0"],
+            avatar: "/frontend/assets/react.svg",
+            avatarAltText: "player-avatar"
+        },
+        {
+            name: "Player 2",
+            vc: "",
+            properties: ["6", "9"],
+            avatar: "/frontend/assets/react.svg",
+            avatarAltText: "player-avatar"
+        },
+        {
+            name: "Player 3",
+            vc: "",
+            properties: ["", "", "", ""],
+            avatar: "/frontend/assets/react.svg",
+            avatarAltText: "player-avatar"
+        },
+        {
+
+            name: "You",
+            vc: "[VC]",
+            properties: ["", "", "", "", "", "", "", ""],
+            avatar: "/frontend/assets/react.svg",
+            avatarAltText: "player-avatar"
+        },
+        // Add more player objects as needed
+    ];
+
+
+    return (
+        <div className="player-stats-container">
+            {playerInfo.map((player, index) => (
+                <div className="player-stats" key={index}>
+                    <div className="player-avatar">
+                        <img src={player.avatar} alt={player.avatarAltText} />
+                    </div>
+                    <div className={`player-name ${player.name === 'You' ? 'bold' : ''}`}>
+                        <span>{player.name}</span>
+                    </div>
+                    <div className="player-vc">
+                        <span>{player.vc}</span>
+                    </div>
+                    <div className="player-properties">
+                        {player.properties.map((property, propertyIndex) => (
+                            <div className="player-property" key={propertyIndex}>
+                                <div className="vertical-rectangle">
+                                    <div className="horizontal-rectangle"></div>
+                                    <div className="number-container">
+                                        <span>{property}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default PlayerStats;
--- frontend/pages/BoardTest.tsx
+++ frontend/pages/BoardTest.tsx
@@ -1,4 +1,5 @@
 import Board from "../components/game/Board.tsx";
+import PlayerStats from "../components/game/PlayerStats.tsx";
 import { BoardCell } from "../components/game/BoardCell.tsx";
 import { useState, useEffect, useRef } from 'react'
 
@@ -34,67 +35,68 @@ export default function Test() {
             <div className="game-session-container">
                 <div className="game-session">
                     {/* <> */}
-                        <div style={{ width: '100vw', height: '100vh' }} ref={container}>
+                    <div style={{ width: '100vw', height: '100vh' }} ref={container}>
 
-                            <Board
-                                width={Math.min(dim.width, dim.height)}
-                                height={Math.min(dim.width, dim.height)}
-                                left={
-                                    [
-                                        { type: "property", price: 140, color: "pink", title: "St. Charles Place" },
-                                        { type: "electric-company", price: 150 },
-                                        { type: "property", price: 140, color: "pink", title: "States Ave" },
-                                        { type: "property", price: 160, color: "pink", title: "Virginia Ave" },
-                                        { type: "railroad", price: 200, title: "Pennsylvania Railroad" },
-                                        { type: "property", price: 180, color: "orange", title: "St. James Place" },
-                                        { type: "chest" },
-                                        { type: "property", price: 180, color: "orange", title: "Tennessee Ave." },
-                                        { type: "property", price: 200, color: "orange", title: "New York Ave." }
-                                    ]
-                                }
-                                right={
-                                    [
-                                        { type: "property", price: 300, color: "green", title: "Pacific Ave." },
-                                        { type: "property", price: 300, color: "green", title: "North Carolina Ave." },
-                                        { type: "chest" },
-                                        { type: "property", price: 320, color: "green", title: "Pennsylvania Ave." },
-                                        { type: "railroad", price: 200, title: "Short Line" },
-                                        { type: "chance" },
-                                        { type: "property", price: 350, color: "blue", title: "Park Place" },
-                                        { type: "luxury-tax", description: "Pay $100" },
-                                        { type: "property", price: 400, color: "blue", title: "Boardwalk" }
-                                    ]
-                                }
-                                top={
-                                    [
-                                        { type: "property", price: 220, color: "red", title: "Kentucky Ave." },
-                                        { type: "chance" },
-                                        { type: "property", price: 220, color: "red", title: "Indiana Ave." },
-                                        { type: "property", price: 240, color: "red", title: "Illinois Ave." },
-                                        { type: "railroad", price: 200, title: "B & O Railroad" },
-                                        { type: "property", price: 260, color: "yellow", title: "Atlantic Ave." },
-                                        { type: "property", price: 260, color: "yellow", title: "Ventnor Ave." },
-                                        {type: "water-works", price: 150},
-                                        { type: "property", price: 280, color: "yellow", title: "Marvin Gardens" }
-                                    ]
-                                }
-                                bottom={
-                                    [
-                                        { type: "property", price: 60, color: "brown", title: "Mediterranean Ave." },
-                                        { type: "chest" },
-                                        { type: "property", price: 60, color: "brown", title: "Baltic Ave." },
-                                        { type: "income-tax", price: 400, description: "Pay $200" },
-                                        { type: "railroad", price: 200, title: "Reading Railroad" },
-                                        { type: "property", price: 100, color: "sky", title: "Oriental Ave." },
-                                        { type: "chance" },
-                                        { type: "property", price: 100, color: "sky", title: "Vermont Ave." },
-                                        { type: "property", price: 120, color: "sky", title: "Connecticut Ave." }
-                                    ]
-                                }
-                            />
-                        </div>
+                        <Board
+                            width={Math.min(dim.width, dim.height)}
+                            height={Math.min(dim.width, dim.height)}
+                            left={
+                                [
+                                    { type: "property", price: 140, color: "pink", title: "St. Charles Place" },
+                                    { type: "electric-company", price: 150 },
+                                    { type: "property", price: 140, color: "pink", title: "States Ave" },
+                                    { type: "property", price: 160, color: "pink", title: "Virginia Ave" },
+                                    { type: "railroad", price: 200, title: "Pennsylvania Railroad" },
+                                    { type: "property", price: 180, color: "orange", title: "St. James Place" },
+                                    { type: "chest" },
+                                    { type: "property", price: 180, color: "orange", title: "Tennessee Ave." },
+                                    { type: "property", price: 200, color: "orange", title: "New York Ave." }
+                                ]
+                            }
+                            right={
+                                [
+                                    { type: "property", price: 300, color: "green", title: "Pacific Ave." },
+                                    { type: "property", price: 300, color: "green", title: "North Carolina Ave." },
+                                    { type: "chest" },
+                                    { type: "property", price: 320, color: "green", title: "Pennsylvania Ave." },
+                                    { type: "railroad", price: 200, title: "Short Line" },
+                                    { type: "chance" },
+                                    { type: "property", price: 350, color: "blue", title: "Park Place" },
+                                    { type: "luxury-tax", description: "Pay $100" },
+                                    { type: "property", price: 400, color: "blue", title: "Boardwalk" }
+                                ]
+                            }
+                            top={
+                                [
+                                    { type: "property", price: 220, color: "red", title: "Kentucky Ave." },
+                                    { type: "chance" },
+                                    { type: "property", price: 220, color: "red", title: "Indiana Ave." },
+                                    { type: "property", price: 240, color: "red", title: "Illinois Ave." },
+                                    { type: "railroad", price: 200, title: "B & O Railroad" },
+                                    { type: "property", price: 260, color: "yellow", title: "Atlantic Ave." },
+                                    { type: "property", price: 260, color: "yellow", title: "Ventnor Ave." },
+                                    { type: "water-works", price: 150 },
+                                    { type: "property", price: 280, color: "yellow", title: "Marvin Gardens" }
+                                ]
+                            }
+                            bottom={
+                                [
+                                    { type: "property", price: 60, color: "brown", title: "Mediterranean Ave." },
+                                    { type: "chest" },
+                                    { type: "property", price: 60, color: "brown", title: "Baltic Ave." },
+                                    { type: "income-tax", price: 400, description: "Pay $200" },
+                                    { type: "railroad", price: 200, title: "Reading Railroad" },
+                                    { type: "property", price: 100, color: "sky", title: "Oriental Ave." },
+                                    { type: "chance" },
+                                    { type: "property", price: 100, color: "sky", title: "Vermont Ave." },
+                                    { type: "property", price: 120, color: "sky", title: "Connecticut Ave." }
+                                ]
+                            }
+                        />
+                    </div>
                     {/* </> */}
-                    <GameCommsPanel socket={socket}/>
+                    <GameCommsPanel socket={socket} />
+                    <PlayerStats />
                     {/* <VoiceChatRoom />
                     <ChatBox game_id={'0'} socket={socket} /> */}
                 </div>
